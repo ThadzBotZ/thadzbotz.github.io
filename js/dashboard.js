50fa07c968478596b897009858c8175d69815d2f
Thadz BotZ <76085092+ThadzBotZ@users.noreply.github.com>
2022-07-06 22:18:50 +0700

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -30,7 +30,7 @@ xhr.send();
 // Visitor
 function getvisitor(){
     var xhr = new XMLHttpRequest();
-    var url = 'https://api.countapi.xyz/hit/web/thadzbotz/visits';
+    var url = 'https://api.countapi.xyz/hit/web/thadzbotz.herokuapp.com';
     xhr.onloadend = function(){
     data = JSON.parse(this.responseText);
     document.getElementById("visits").textContent = data.value
